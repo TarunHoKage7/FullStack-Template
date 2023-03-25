892ed631f2c57c556593b95471286922fbf13a94
UserTarun <saikalyantarun2001@gmail.com>
2023-03-25 12:52:15 +0530

    added variables and mongo connection

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 const app = express();
 require("dotenv").config();
+
+//variables
+const db = "",
+    dbConnectionString = process.env.DB_STRING,
+    dbName = "sample_mflix",
+    collection = "";
+
+//connect to mongo
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log("Connected to database");
+        db = client.db(dbName);
+        collection = db.collection("movies");
+    });
